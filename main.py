5b34ef8a06093d4eb431fda9da1817e60f88a6a6
Isaac Akalanne <iakalann@jaguarlandrover.com>
2023-01-13 21:55:11 +0000

    Initial manual PPO implementation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from gameenv import GameEnv
 
 env = GameEnv()
--- ppo.py
+++ ppo.py
@@ -1,22 +1,93 @@
-import torch
+import os
+
+import numpy as np
+import torch as T
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
+from torch.distributions.categorical import Categorical
 from gameenv import GameEnv
 
+class PPO_Memory:
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = []
+        self.vals = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
 
-def get_action(state, agent):
-    action_probs = agent(state)
-    action_probs = torch.softmax(action_probs, dim=-1)
-    return action_probs
+        self.batch_size = batch_size
 
+    def generate_batches(self):
+        n_states = len(self.states)
+        batch_start = np.arange(0, n_states, self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(indices)
+        batches = [indices[i:i+self.batch_size] for i in batch_start]
 
-def select_action(state, agent):
-    state = torch.FloatTensor(state).unsqueeze(0)
-    action_probs = agent(state)
-    action_probs = torch.softmax(action_probs, dim=-1)
-    action = action_probs.multinomial(num_samples=1).item()
-    return action
+        return np.array(self.states),\
+            np.array(self.actions),\
+            np.array(self.probs),\
+            np.array(self.vals),\
+            np.array(self.rewards),\
+            np.array(self.dones),\
+            batches
+
+    def store_memory(self, state, action, probs, vals, reward, done):
+        self.states.append(state)
+        self.actions.append(action)
+        self.probs.append(probs)
+        self.vals.append(vals)
+        self.rewards.append(reward)
+        self.dones.append(done)
+
+    def clear_memory(self):
+        self.states = []
+        self.probs = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.vals = []
+
+class ActorNetwork(nn.Module):
+
+    def __init__(self, n_actions, input_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
+        super(ActorNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+        self.actor = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax(dim=-1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor(state)
+        dist = Categorical(dist)
+
+        return dist
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class CriticNetwork(nn.Module)
+
+    def __init__(self, input_dims, alpha, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
+        super(CriticNetwork, self).__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'critic_torch_ppo')
+        self.critic = nn.Sequential(
+            nn.
+        )
 
 
 class PPOAgent(nn.Module):
